48407f20cb6409e6058419569581d1d98c2ebee0
akakkar2006 <akakkar2006@gmail.com>
2020-09-10 19:44:52 -0400

    Made the game synchronized so that it can be seen the same in multiple tabs

--- sketch.js
+++ sketch.js
@@ -1,29 +1,50 @@
 var ball;
-
+var database;
+var position;
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    database=firebase.database();
+    var ballReferance=database.ref('ball/position');
+    ballReferance.on('value',readPosition,showError);
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+    if(position!==undefined){
+        if(keyDown(LEFT_ARROW)){
+            changePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            changePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            changePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            changePosition(0,+1);
+        }
+        drawSprites();
     }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+ database.ref('ball/position').update({
+     'x': ball.x+x,
+     'y': ball.y+y
+ })   
 }
+
+function readPosition(data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+
+
+}
+function showError(){
+
+}
+
+
